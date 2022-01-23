34da7a5025a1a943ff87ffa4a75894799f1206f6
anandubale <au26199@gmail.com>
2022-01-23 12:14:30 +0530

    renamed salary to  basic pay and added columns deductions, tazable_pay, tax and Net_pay

--- UC1.sql
+++ UC1.sql
@@ -4,6 +4,15 @@ alter table employee_payroll1 add address Varchar(255) not null default 'TBD'
 alter table employee_payroll1 add Department Varchar(255) not null default 'non-Def' 
 
 
+exec sp_rename 'employee_payroll1.salary', 'Basic_pay', 'column'
+alter table employee_payroll1 add Deductions Money 
+alter table employee_payroll1 add Taxable_pay Money 
+alter table employee_payroll1 add Tax Money 
+alter table employee_payroll1 add NetPay Money 
+
+
+
+
 
 select* 
 from employee_payroll1
