1babcca835da3db19ea6238be65b8d9dd031feef
anandubale <au26199@gmail.com>
2022-01-30 11:18:35 +0530

    normalization of data for simplicity with small tables

--- Payroll_service/SQLQuery1.sql
+++ Payroll_service/SQLQuery1.sql
@@ -1,12 +1,39 @@
 select*
 from employee_payroll1
 
+	create table Taxable
+	(
+		tax_id int not null default ('1') primary key,
+		Deductions int not null,
+		Taxable_Pay int not null ,
+		tax int not null
+		)
+
+select*
+from Taxable
 
 
+	create table PayrollDetails
+	(
+	Payroll_id int not null default ('01') Primary Key,
+	Basic_Pay int not null,
+	NetPay int not null,
+	tax_id int not  null default ('1') foreign key references Taxable(tax_id)
+	)
 
-insert into employee_payroll1
-(name, Basic_Pay, startDate, Gender,phoneNumber,address, Department, Deductions, Taxable_Pay, Tax,NetPay) VALUES
-('Terisa',40000.00, '2005-01-01','F',9238423984,'newyard','Marketing',1000.00, 34000.00, 500.00,43000);
+select*
+from PayrollDetails
+	
 
-insert into employee_payroll1 
-VALUES('Terisa',40000.00, '2005-01-01','F',9238423984,'newyard','sales',1000.00, 34000.00, 500.00,43000);
+
+	create table Contact_Details
+	(
+		name varchar(20) not null,
+		phoneNumber varchar(12) not null,
+		Address varchar(max) not null,
+		Payroll_id int not null default ('01') foreign key references PayrollDetails(Payroll_id)
+			
+	)
+
+select*
+from Contact_Details
